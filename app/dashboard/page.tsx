ffc00c278f863e7309ea2f05ceeef33cfa8b4bea
Faizanahmed Saiyed <saiyed.faizanahmed1@gmail.com>
2023-11-19 06:22:42 +0530

    learned tailwind responsive design

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="grid grid-cols-2  gap-4">
+        <p className="  text-center text-orange-800">Dashboard Page</p>
+        <p className="text-center text-orange-800">Dashboard Page</p>
+        <p className="text-center text-orange-800">Dashboard Page</p>
+        <p className="text-center text-orange-800">Dashboard Page</p>
+        <p className="text-center text-orange-800">Dashboard Page</p>
+        <p className="text-center text-orange-800">Dashboard Page</p>
+        <p className="text-center text-orange-800">Dashboard Page</p>
+        <p className="text-center text-orange-800">Dashboard Page</p>
+      </div>
+    </>
+  );
 };
 
 export default page;
